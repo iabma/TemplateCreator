f32ba02ce0e2015666a57bc727151cbd1194f568
Ian Balaguera <balagueraian@gmail.com>
2019-03-21 21:22:33 -0400

    Bug fixes + import implementation

--- formatter.js
+++ formatter.js
@@ -1,10 +1,17 @@
 let input = document.getElementsByTagName("textarea")[0],
     output = document.getElementById("out");
 
-var dropMenu = [{ name: "Create Dropdown", fn: add }],
-    optionMenu = [{ name: "Add Option", fn: option }];
+var dropMenu = [{ name: "Create Dropdown", fn: add },
+{ name: "Delete", fn: remove }],
+    divDropMenu = [{ name: "Create Dropdown", fn: divAdd },
+    { name: "Delete", fn: remove }],
+    optionMenu = [{ name: "Add Option", fn: option },
+    { name: "Delete", fn: remove }];
 
-let addDropMenu = new ContextMenu('.content', dropMenu, {
+let addDropMenu = new ContextMenu('.span', dropMenu, {
+    className: "context-menu"
+}),
+    addDivDropMenu = new ContextMenu('.content', divDropMenu, {
         className: "context-menu"
     }),
     addOptionMenu = new ContextMenu('.drop', optionMenu, {
@@ -26,7 +33,381 @@ var formatted = {},
 let backgroundColor = d3.scaleLinear()
     .domain([0, 15])
     .range(["rgb(220, 220, 220)", "rgb(24, 24, 24)"])
-    .interpolate(d3.interpolateHcl)
+    .interpolate(d3.interpolateHcl),
+    divBackgroundColor = d3.scaleLinear()
+        .domain([0, 15])
+        .range(["rgb(220, 190, 190)", "rgb(24, 0, 0)"])
+        .interpolate(d3.interpolateHcl);
+
+document.getElementById("json").addEventListener("click", () => {
+    var range = document.createRange();
+    range.selectNode(document.getElementById("json"));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+});
+
+document.getElementById("import").addEventListener("change", read, false);
+
+function read(e) {
+    var file = e.target.files[0];
+    if (!file) return;
+    var reader = new FileReader();
+    reader.onload = (e) => {
+        var content = e.target.result;
+        generateTree(JSON.parse(content));
+    };
+    reader.readAsText(file);
+}
+
+function generateTree(content) {
+    newTemplate(content.name);
+    formatted.name = content.name;
+    Object.keys(content).forEach(childName => {
+        if (childName != "name") {
+            newType(childName);
+            generateType(childName, content[childName], document.getElementById(childName))
+        }
+    });
+}
+
+function generateType(name, type, parent) {
+    let content = parent.children[2];
+    content.removeChild(content.children[0]);
+    type.content.forEach(child => {
+        if (child.type == "text") {
+            let text = document.createElement("div");
+            text.className = "span";
+            text.contentEditable = true;
+            text.innerHTML = child.content;
+            text.addEventListener("keyup", () => {
+                updateContent(parent.id)
+            });
+            content.appendChild(text);
+        } else {
+            generateDrop(child, content);
+        }
+    });
+    updateContent(name);
+}
+
+function generateDrop(tree, parent) {
+    let drop = document.createElement("div")
+    drop.contentEditable = false;
+    drop.className = "drop";
+    parent.appendChild(drop);
+    drop.style.background = backgroundColor(determineDepth(drop));
+    tree.content.forEach(opt => {
+        if (opt.type == "text") {
+            option(drop, opt.content);
+        } else {
+            let container = document.createElement("div");
+            container.className = "content";
+            drop.appendChild(container);
+            generateProgression(opt, container);
+            container.style.backgroundColor = divBackgroundColor(determineDepth(container));
+        }
+    })
+}
+
+function generateProgression(tree, parent) {
+    tree.content.forEach(element => {
+        if (element.type == "text") {
+            let text = document.createElement("div");
+            text.className = "span";
+            text.contentEditable = true;
+            text.innerHTML = element.content;
+            text.addEventListener("keyup", () => {
+                updateContent(parent.closest(".template").id)
+            });
+            parent.appendChild(text);
+        } else {
+            generateDrop(element, parent);
+        }
+    });
+}
+
+function cleanUpSpanText(span) {
+    var clean = span.innerHTML;
+    if (span.children.length > 0) {
+        var newText = "";
+        Array.from(span.children).forEach(div => {
+            newText += div.innerHTML == "<br>" ? "" : div.innerHTML.replace("<br>", "");
+            newText += "\n";
+        });
+        clean = newText.substring(0, newText.length - 1);
+    }
+    return clean;
+}
+
+function searchOpt(div) {
+    let opt = {
+        type: "progression",
+        content: []
+    };
+    if (div.children.length == 1 && div.children[0].className == "span") {
+        var c = "";
+        if (div.children[0].innerHTML != "" && div.children[0].innerHTML != "<br>")
+            c = cleanUpSpanText(div.children[0]);
+        let text = {
+            type: "text",
+            content: c
+        };
+        return text;
+    }
+    Array.from(div.children).forEach(child => {
+        if (child.className == "span" && child.innerHTML != "" && child.innerHTML != "<br>") {
+            let text = {
+                type: "text",
+                content: cleanUpSpanText(child)
+            };
+            opt.content.push(text);
+        } else if (child.className == "drop") {
+            opt.content.push(searchDropdown(child));
+        }
+    });
+    return opt;
+}
+
+function searchDropdown(content) {
+    let obj = {
+        type: "dropdown",
+        content: []
+    }
+    Array.from(content.children).forEach(div => {
+        obj.content.push(searchOpt(div))
+    });
+    return obj;
+}
+
+function updateContent(typeName) {
+    let root = document.getElementById(typeName).getElementsByClassName("content")[0];
+    var JSONContent = [];
+    Array.from(root.children).forEach(child => {
+        if (child.className == "span" && child.innerHTML != "" && child.innerHTML != "<br>") {
+            let text = {
+                type: "text",
+                content: cleanUpSpanText(child)
+            };
+            JSONContent.push(text);
+        } else if (child.className == "drop") {
+            JSONContent.push(searchDropdown(child));
+        }
+    });
+    if (formatted[typeName])
+        formatted[typeName].content = JSONContent;
+    updateJSON();
+}
+
+function remove(element) {
+    let parent = element.parentElement
+    if (parent.parentElement.className == "template" && parent.className != "template") {
+        parent.removeChild(element);
+        updateContent(parent.closest(".template").id);
+    } else if (parent.className != "template") {
+        parent.parentElement.removeChild(parent);
+        updateContent(parent.parentElement.closest(".template").id);
+    }
+}
+
+function add(span) {
+    let parentDiv = span.parentElement;
+    let selected = document.getSelection().getRangeAt(0);
+    let info = selected.toString();
+    selected.insertNode(document.createTextNode("\u0001"));
+    selected.collapse(false);
+    selected.insertNode(document.createTextNode("\u0001"));
+    var prevContent = span.innerHTML.split("\u0001");
+
+    parentDiv.removeChild(span);
+    let firstSpan = document.createElement("div");
+    firstSpan.className = "span";
+    firstSpan.contentEditable = true;
+    firstSpan.innerHTML = prevContent[0];
+    firstSpan.addEventListener("keyup", () => {
+        updateContent(parentDiv.closest(".template").id)
+    });
+    parentDiv.appendChild(firstSpan);
+    let drop = document.createElement("div")
+    drop.contentEditable = false;
+    drop.className = "drop";
+    parentDiv.appendChild(drop)
+    drop.style.background = backgroundColor(determineDepth(drop));
+    option(drop, info);
+    let secondSpan = document.createElement("div");
+    secondSpan.className = "span";
+    secondSpan.contentEditable = true;
+    secondSpan.innerHTML = prevContent[2];
+    secondSpan.addEventListener("keyup", () => {
+        updateContent(parentDiv.closest(".template").id)
+    });
+    parentDiv.appendChild(secondSpan);
+
+    updateContent(parentDiv.closest(".template").id);
+}
+
+function divAdd(div) {
+    let drop = document.createElement("div")
+    drop.contentEditable = false;
+    drop.className = "drop";
+    div.appendChild(drop)
+    drop.style.background = backgroundColor(determineDepth(drop));
+    option(drop, "");
+    let afterSpan = document.createElement("div");
+    afterSpan.className = "span";
+    afterSpan.contentEditable = true;
+    afterSpan.addEventListener("keyup", () => {
+        updateContent(div.closest(".template").id)
+    });
+    div.appendChild(afterSpan)
+    updateContent(div.closest(".template").id)
+}
+
+function determineDepth(element) {
+    let current = element,
+        depth = 0;
+    while (current.className != "template") {
+        current = current.parentElement;
+        depth++;
+    }
+    return depth;
+}
+
+function option(div, info) {
+    let optionContainer = document.createElement("div");
+    optionContainer.className = "content";
+    let option = document.createElement("div");
+    option.className = "span";
+    option.contentEditable = true;
+    option.innerHTML = info ? info : "";
+    option.addEventListener("keyup", () => {
+        updateContent(div.closest(".template").id)
+    });
+    optionContainer.appendChild(option);
+    div.appendChild(optionContainer);
+    optionContainer.style.backgroundColor = divBackgroundColor(determineDepth(optionContainer));
+}
+
+function newTemplate(text) {
+    document.getElementById("template").style.visibility = "hidden";
+    let existingTemplate = document.getElementById("mainTemplate")
+    if (existingTemplate != null)
+        existingTemplate.remove();
+    let remove = document.createElement("div");
+    remove.className = "create";
+    remove.innerHTML = "x";
+    let addType = document.createElement("div");
+    addType.className = "create";
+    addType.innerHTML = "add type";
+    addType.addEventListener("click", () => {
+        newType();
+    });
+    let field = document.createElement("div");
+    field.contentEditable = true;
+    field.className = "field";
+    field.innerHTML = text != null ? text : "template name (e.g. WBC)";
+    let template = document.createElement("div");
+    template.className = "template";
+    template.id = "mainTemplate";
+    template.style.background = backgroundColor(0);
+    template.appendChild(remove);
+    template.appendChild(addType);
+    template.appendChild(field);
+    output.appendChild(template);
+    field.focus();
+    if (text != null) {
+        formatted.name = text;
+        updateJSON();
+    }
+    field.addEventListener("keyup", (key) => {
+        formatted.name = cleanUpSpanText(field);
+        updateJSON();
+    });
+    field.onclick = () => {
+        if (field.innerHTML == "template name (e.g. WBC)") {
+            field.innerHTML = "";
+        }
+    }
+    remove.addEventListener("click", () => {
+        formatted = {};
+        template.remove();
+        document.getElementById("template").style.visibility = "visible";
+        updateJSON();
+    });
+}
+
+function newType(text) {
+    let remove = document.createElement("div");
+    remove.className = "create";
+    remove.innerHTML = "x";
+    let field = document.createElement("div");
+    field.contentEditable = true;
+    field.className = "field";
+    field.innerHTML = text != null ? text : "type (i.e. English)";
+    let container = document.createElement("div");
+    container.className = "content";
+    container.tabIndex = 0;
+    container.style.background = divBackgroundColor(1);
+    let content = document.createElement("div");
+    content.className = "span"
+    content.tabIndex = 0;
+    content.contentEditable = true;
+    content.innerHTML = "Enter content here.";
+    content.onclick = () => {
+        if (content.innerHTML == "Enter content here.")
+            content.innerHTML = "";
+    };
+    let type = document.createElement("div");
+    type.className = "template";
+    type.id = field.innerHTML;
+    type.name = "type";
+    type.style.background = divBackgroundColor(0);
+    container.appendChild(content)
+    type.appendChild(remove);
+    type.appendChild(field);
+    type.appendChild(container);
+    document.getElementById("mainTemplate").appendChild(type);
+    field.focus();
+    var prev,
+        prevContent = [],
+        name;
+    field.onclick = () => {
+        if (field.innerHTML == "type (i.e. English)") {
+            field.innerHTML = "";
+        }
+    }
+    field.addEventListener("keyup", () => {
+        name = cleanUpSpanText(field);
+        type.id = name;
+        if (prev != null) {
+            if (prevContent.length > 0) {
+                prevContent = formatted[prev].content;
+            }
+            delete formatted[prev];
+        }
+        formatted[name] = {
+            content: prevContent
+        };
+        prev = name;
+        updateContent(type.id);
+    });
+    content.addEventListener("keyup", () => {
+        updateContent(type.id);
+    })
+    remove.addEventListener("click", () => {
+        delete formatted[name];
+        updateJSON();
+        type.remove();
+    })
+    if (text != null) {
+        formatted[text] = {
+            content: []
+        };
+    }
+}
+
+function updateJSON() {
+    document.getElementById("json").innerHTML = JSON.stringify(formatted, undefined, 4);
+}
 
 /* function search(id, tree) {
     let returnVal = null;
@@ -127,178 +508,6 @@ function addProgression(tree, id) {
         }];
     }
 } */
-
-function searchOpt(content, i) {
-    let opt = {
-        type: "progression",
-        content: []
-    }
-    let str = content[i++];
-    while (str != "/div") {
-        if (str == 'div contenteditable="false" class="drop" style="background: rgb(176, 176, 176);"') {
-            let ret = searchDropdown(content, i);
-            i = ret[0] - 1;
-            opt.content.push(ret[1]);
-        } else if (str.length > 0 && str != " " && str != "/div") {
-            opt.content.push(str);
-        }
-        str = content[i++];
-    }
-    if (opt.content.length == 1 && typeof opt.content[0] === "string") {
-        return [i, opt.content[0]]
-    } else {
-        return [i, opt];
-    }
-}
-
-function searchDropdown(content, i) {
-    let obj = {
-        type: "dropdown",
-        content: []
-    }
-    let str = content[i++];
-    while (str != "/div") {
-        if (str == 'div class="content" contenteditable="true"') {
-            let ret = searchOpt(content, i);
-            i = ret[0];
-            obj.content.push(ret[1])
-        }
-        str = content[i++];
-    }
-    return [i, obj];
-}
-
-function updateContent(typeName) {
-    let content = document.getElementById(typeName).getElementsByClassName("content")[0].innerHTML.split(/[<>]/);
-    var JSONContent = [];
-    console.log(content)
-    for (var i = 0; i < content.length; i++) {
-        if (content[i] == 'div contenteditable="false" class="drop" style="background: rgb(176, 176, 176);"') {
-            let ret = searchDropdown(content, i);
-            i = ret[0] - 1;
-            JSONContent.push(ret[1]);
-        } else if (content[i].length > 0 && content[i] != " " && content[i] != "/div") {
-            JSONContent.push(content[i]);
-        }
-    }
-    if (formatted[typeName])
-        formatted[typeName].content = JSONContent;
-    updateJSON();
-}
-
-function add(div) {
-    console.log(div.name)
-    let selected = document.getSelection().getRangeAt(0);
-    let info = selected.toString();
-    selected.insertNode(document.createTextNode("\u0001"));
-    selected.collapse(false);
-    selected.insertNode(document.createTextNode("\u0001"));
-    var prevContent = div.innerHTML.split("\u0001");
-    prevContent[1] = addDrop(info);
-    div.innerHTML = prevContent.join("");
-    updateContent(div.closest(".template").id);
-}
-
-function option(div, info) {
-    let opt = document.createElement("div");
-    opt.className = "content";
-    opt.contentEditable = true;
-    opt.innerHTML = info ? info : "";
-    opt.addEventListener("keyup", () => {
-        updateContent(div.closest(".template").id)
-    })
-    div.appendChild(opt);
-}
-
-function addDrop(info, div) {
-    let drop = document.createElement("div")
-    drop.contentEditable = false;
-    drop.className = "drop";
-    drop.style.background = backgroundColor(3);
-    option(drop, info);
-    return drop.outerHTML;
-}
-
-function newTemplate() {
-    document.getElementById("template").remove();
-    let remove = document.createElement("div");
-    remove.className = "create";
-    remove.innerHTML = "x";
-    remove.addEventListener("click", () => {
-        template.remove();
-    })
-    let addType = document.createElement("div");
-    addType.className = "create";
-    addType.innerHTML = "add type";
-    addType.addEventListener("click", () => {
-        newType();
-    });
-    let field = document.createElement("textarea");
-    field.className = "field";
-    field.placeholder = "template name (e.g. WBC)";
-    let template = document.createElement("div");
-    template.className = "template";
-    template.id = "mainTemplate";
-    template.style.background = backgroundColor(0);
-    template.appendChild(remove);
-    template.appendChild(addType);
-    template.appendChild(field);
-    output.appendChild(template);
-    field.focus();
-    field.addEventListener("keyup", (key) => {
-        formatted.name = field.value;
-        updateJSON();
-    });
-}
-
-function newType() {
-    var depth = 1;
-    let remove = document.createElement("div");
-    remove.className = "create";
-    remove.innerHTML = "x";
-    let field = document.createElement("textarea");
-    field.className = "field";
-    field.placeholder = "type (i.e. English)";
-    let content = document.createElement("div");
-    content.className = "content";
-    content.innerHTML = ""
-    content.tabIndex = 0;
-    content.contentEditable = true;
-    content.name = depth;
-    let type = document.createElement("div");
-    type.className = "template";
-    type.id = field.value;
-    type.name = "type";
-    type.style.background = backgroundColor(depth);
-    type.appendChild(remove);
-    type.appendChild(field);
-    type.appendChild(content);
-    document.getElementById("mainTemplate").appendChild(type);
-    field.focus();
-    var prev,
-        prevContent = [];
-    field.addEventListener("keyup", () => {
-        type.id = field.value;
-        if (prev != null) {
-            if (prevContent.length > 0) {
-                prevContent = formatted[prev].content;
-            }
-            delete formatted[prev];
-        }
-        formatted[field.value] = {
-            content: prevContent
-        };
-        prev = field.value;
-        updateContent(type.id);
-    });
-    content.addEventListener("keyup", () => {
-        updateContent(type.id);
-    })
-    remove.addEventListener("click", () => {
-        delete search(type.id, formatted);
-        type.remove();
-    })
-}
 
 /* function newString(parent, name, depth) {
     let remove = document.createElement("div");
@@ -452,17 +661,6 @@ function newProgression(parent, name, depth) {
         updateJSON();
     })
 } */
-
-function updateJSON() {
-    document.getElementById("json").innerHTML = JSON.stringify(formatted, undefined, 4);
-}
-
-document.getElementById("json").addEventListener("click", () => {
-    var range = document.createRange();
-    range.selectNode(document.getElementById("json"));
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-});
 
 /* input.addEventListener("click", () => {
     input.value = "";
